6669bfaafce8a4133c3695be2da37f0391fac79e
nabendu82 <nabendu.biswas@gmail.com>
2022-03-22 10:05:51 +0530

    Day 9-React

---
+++ src/components/Greeting.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Greeting = () => {
+    return (
+        <h1>Greeting from React</h1>
+    )
+}
+
+export default Greeting
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Welcome extends Component {
+    render() {
+        return (
+            <p>Welcome to React</p>
+        )
+    }
+}
+
+export default Welcome
